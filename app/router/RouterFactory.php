924a1deac9974494be145146faabeb9f8704f646
urbanfi4 <urbanfi4@fit.cvut.cz>
2020-09-02 16:44:04 +0200

    router edited

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -24,10 +24,10 @@ class RouterFactory
 
 		// Basic Routes
         $project[] = new Route('[<lang=cs [a-z]{2}>/]', 'Homepage:default');
-        $project[] = new Route('[<lang=cs [a-z]{2}>/]galerie-<year>', 'Homepage:gallery');
-        $project[] = new Route('[<lang=cs [a-z]{2}>/]vysledky-<year>', 'Homepage:results');
+        $project[] = new Route('[<lang=cs [a-z]{2}>/]galerie[/<year='. date('Y'). '>]', 'Homepage:gallery');
+        $project[] = new Route('[<lang=cs [a-z]{2}>/]vysledky[/<year='. date('Y'). '>]', 'Homepage:results');
         $project[] = new Route('[<lang=cs [a-z]{2}>/]novinky', 'New:default');
-        $project[] = new Route('[<lang=cs [a-z]{2}>/]<slug>', 'New:show');
+        $project[] = new Route('[<lang=cs [a-z]{2}>/]novinka/<slug>', 'New:show');
         $project[] = new Route('[<lang=cs [a-z]{2}>/]kalendar', 'Event:default');
 
         $project[] = new Route('[<lang=cs [a-z]{2}>/]<presenter>/<action>', 'Error:404');
