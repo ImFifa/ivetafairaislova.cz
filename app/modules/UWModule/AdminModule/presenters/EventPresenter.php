8ed1d6e85d3534173a36bd1a759aaf8ce5cb880e
Filip Urban <urbanfi4@fit.cvut.cz>
2020-09-04 03:31:06 +0200

    events error + css

--- app/modules/UWModule/AdminModule/presenters/EventPresenter.php
+++ app/modules/UWModule/AdminModule/presenters/EventPresenter.php
@@ -20,25 +20,27 @@ class EventPresenter extends BaseUWPresenter
 
     public function renderEdit(?int $id): void
 	{
-		$event = $this->repository->event->get($id);
+        if ($id !== null && $event = $this->repository->event->get($id)) {
+            $event = $event->toArray();
 
-        $event = $event->toArray();
+            /** @var DateTime $date_from */
+            $date_from = $event['date_from'];
+            $event['date_from'] = $date_from->format('j.n.Y');
 
-        /** @var DateTime $date_from */
-        $date_from = $event['date_from'];
-        $event['date_from'] = $date_from->format('d.m.Y');
+            if ($event['date_to'] != NULL) {
+                /** @var DateTime $date_to */
+                $date_to = $event['date_to'];
+                $event['date_to'] = $date_to->format('j.n.Y');
+            }
 
-        if ($event['date_to'] != NULL) {
-            /** @var DateTime $date_to */
-            $date_to = $event['date_to'];
-            $event['date_to'] = $date_to->format('d.m.Y');
+            /** @var Form $form */
+            $form = $this['eventForm'];
+            $form->setDefaults($event);
+
+            $this->template->event = $event;
+        } else {
+            $this->template->event = null;
         }
-
-        /** @var Form $form */
-        $form = $this['eventForm'];
-        $form->setDefaults($event);
-
-		$this->template->event = $event;
 	}
 
 	protected function createComponentEventGrid(): EventGrid
@@ -57,10 +59,11 @@ class EventPresenter extends BaseUWPresenter
             ->setRequired('Musíte uvést jméno události');
 
         $form->addText('date_from', 'Datum zahájení')
+            ->setDefaultValue((new DateTime())->format('j.n.Y'))
             ->setRequired('Musíte uvést plánovaný počátek události.');
 
         $form->addText('date_to', 'Datum ukončení')
-            ->setDefaultValue((new DateTime())->format('Y.m.d'));
+            ->setDefaultValue((new DateTime())->format('j.n.Y'));
 
         $form->addText('link', 'Odkaz')
             ->addRule(Form::MAX_LENGTH, 'Maximálné délka je %s znaků', 200);
@@ -69,6 +72,8 @@ class EventPresenter extends BaseUWPresenter
 
         $form->onSubmit[] = function (Form $form) {
             $values = $form->getValues(true);
+            $values['date_from'] = date_create_from_format('j.n.Y', $values['date_from'])->setTime(0, 0, 0);
+            $values['date_to'] = date_create_from_format('j.n.Y', $values['date_to'])->setTime(0, 0, 0);
 
             if ($values['id'] === '') {
                 unset($values['id']);
